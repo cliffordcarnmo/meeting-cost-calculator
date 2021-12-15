ec2aff8b54145233ea864d31dd384cc1a76dd0d3
Mackain <markus.j.lindgren@gmail.com>
2021-12-15 15:49:34 +0100

    trees!

--- js/report.js
+++ js/report.js
@@ -1,9 +1,15 @@
-
+let meeting = new Meeting();
 window.onload = () => {
     const params = new URLSearchParams(window.location.search);
-    r = JSON.parse(atob(params.get("p")));
-    document.getElementById("participants").innerHTML = r.participants.length;
-    document.getElementById("time").innerHTML = r.time;
-    document.getElementById("cost").innerHTML = "€ " + r.cost;
-    console.log(r);
+    meeting = JSON.parse(atob(params.get("p")));
+    secondsInMeeting = Math.abs(new Date(meeting.startTime) - new Date(meeting.endTime)) /1000;
+    costPerSec = meeting.cost / secondsInMeeting;
+    document.getElementById("participants").innerHTML = meeting.participants.length;
+    document.getElementById("time").innerHTML = meeting.time;
+    document.getElementById("cost").innerHTML = "€ " + meeting.cost;
+    document.getElementById("costPerSec").innerHTML = "€ " + (meeting.cost / secondsInMeeting).toFixed(2);
+    for (let i = 0; i < meeting.cost; i++) {
+        document.getElementById("trees").innerHTML += "🌲";
+    }
+    
 }
